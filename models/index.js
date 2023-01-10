a9bce86a6b2e733467b3acdadad756e0e6944b9f
luke123adams <lxa207@gmail.com>
2023-01-10 14:20:23 +0000

    fixed couscous problem

--- models/index.js
+++ models/index.js
@@ -2,9 +2,16 @@ import {pool} from '../db/index.js'
 
 export async function getProduct (searchTerm) {
 
-    const productQuery = await pool.query('SELECT * FROM foods WHERE product_name = $1;', [searchTerm]);
+    const productQuery = await pool.query('SELECT * FROM foods WHERE product_name ILIKE $1;', [`%${searchTerm}%`]);
     const productArr = productQuery.rows;
-    //console.log(productArr)
+    console.log(productArr[0])
     return productArr[0]
 
+}
+
+export async function getAll () {
+
+    const result = await pool.query('SELECT * FROM foods')
+    console.log(result.rows)
+    return result.rows
 }
