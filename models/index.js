cb1ea0961cc83f1727f4180840f6eb2bc352271f
marthamaybennett <marthamaybennett123@gmail.com>
2023-01-11 11:07:12 +0000

    adding functionality to the search bar

--- models/index.js
+++ models/index.js
@@ -3,9 +3,17 @@ import {pool} from '../db/index.js'
 export async function getProduct (searchTerm) {
 
     const productQuery = await pool.query('SELECT * FROM foods WHERE product_name ILIKE $1;', [`%${searchTerm}%`]);
+    const barcodeQuery = await pool.query('SELECT * FROM foods WHERE barcode_number ILIKE $1;', [`%${searchTerm}%`]);
     const productArr = productQuery.rows;
-    console.log(productArr[0])
-    return productArr[0]
+   
+    if (typeof Number (searchTerm) === Number) {
+        console.log(barcodeQuery.rows)
+        return barcodeQuery.rows
+    }
+
+    else {
+        return productArr
+    }
 
 }
 
@@ -15,3 +23,4 @@ export async function getAll () {
     console.log(result.rows)
     return result.rows
 }
+
