7a9a75d608b99eb9f22e4b6466a529904c2c1f22
Cmndgrab <110107052+Cmndgrab@users.noreply.github.com>
2023-01-11 16:42:45 +0000

    Dev (#4)
    * dev branch setup
    
    * setting up backend
    
    * middleware plated
    
    * cors booted
    
    * updating the backend
    
    * added get request handler, getting esm error msg
    
    * fixed kinda
    
    * connected back and front endfor MVP
    
    * fixed couscous problem
    
    * added .env file
    
    * added to README
    
    * adding functionality to the search bar
    
    * search function works with product name and barcode
    
    * barcode specific search
    
    * added comment
    
    * bug splatted
    
    * okay sure
    
    * still having issues with req handler if statement
    
    * fixed
    
    Co-authored-by: marthamaybennett <marthamaybennett123@gmail.com>
    Co-authored-by: luke123adams <lxa207@gmail.com>
    Co-authored-by: Sunbearian <stigcollins@icloud.com>
    Co-authored-by: luke123adams <112545296+luke123adams@users.noreply.github.com>
    Co-authored-by: violetacf <violeta@cique.es>

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# final-project_back-end
 
-step by step!
+# Final Project Backend
+
+## For Development
+
+    - npm i
+    - create .env file with PORT = '3010' + database  URL 'POSTGRES_CONNECTION_URL'
+
+  ## Database Behaviour
+
+  We are looking for products by name and any requests are not case-sensitive or needing the complete name.  
+
+  ## Database Scripts
+
+  
+## API Reference
+
+#### Get all items
+
+```http
+  GET /api/items
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `api_key` | `string` | **Required**. Your API key |
+
+#### Get item
+
+```http
+  GET /api/items/${id}
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to fetch |
+
+
+
+  ## Backend Deployment Link
+
--- models/index.js
+++ models/index.js
@@ -1,17 +1,39 @@
 import {pool} from '../db/index.js'
 
+
+// I heart github
+
 export async function getProduct (searchTerm) {
 
     const productQuery = await pool.query('SELECT * FROM foods WHERE product_name ILIKE $1;', [`%${searchTerm}%`]);
+    const barcodeQuery = await pool.query('SELECT * FROM foods WHERE barcode_number = $1;', [searchTerm]);
     const productArr = productQuery.rows;
-    console.log(productArr[0])
-    return productArr[0]
+    console.log(searchTerm)
+    function containsOnlyNumbers(str) {
+        return /^\d+$/.test(str);
+      }
+   
+    if (containsOnlyNumbers(searchTerm) == true) {
+        console.log(barcodeQuery.rows)
+        return barcodeQuery.rows
+    }
+
+    else {
+        console.log(productArr)
+        return productArr
+    }
 
 }
+
+
+
 
 export async function getAll () {
 
     const result = await pool.query('SELECT * FROM foods')
     console.log(result.rows)
     return result.rows
+
 }
+
+
